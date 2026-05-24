acbf89846c2e0f628166516b6baf9abed85aa920
Pavel Korotkevich <pawday@mail.ru>
2026-05-24 17:06:06 +0300

    Factor out types to header

--- Example.cc
+++ Example.cc
@@ -3,6 +3,7 @@
 #include <wlalat/Message.hh>
 #include <wlalat/MessageParser.hh>
 #include <wlalat/MessageSerializer.hh>
+#include <wlalat/Types.hh>
 #include <wlalat/Unix/Socket.hh>
 
 #include <sys/socket.h>
@@ -30,69 +31,6 @@ constexpr uint32_t wl_display_object_id = 1;
 constexpr uint32_t wl_display_get_registry_request_id = 1;
 
 namespace wlalat {
-
-struct Numeric
-{
-    Numeric() = default;
-    Numeric(uint_fast32_t v) : _v{v}
-    {
-    }
-
-    int_fast32_t raw() const
-    {
-        return _v;
-    }
-
-protected:
-    uint_fast32_t _v;
-};
-
-struct Int : Numeric
-{
-    operator int_least32_t() const
-    {
-        return _v & 0xffffffffu;
-    }
-};
-
-struct UInt : Numeric
-{
-    operator uint_least32_t() const
-    {
-        return _v & 0xffffffffu;
-    }
-};
-
-struct String : std::basic_string_view<char>
-{
-    String() = default;
-    String(std::basic_string_view<char> s) : std::basic_string_view<char>{s}
-    {
-    }
-};
-
-struct Fixed : Numeric
-{
-    // clang-format off
-    double f() { return to_floating<float>(); }
-    double d() { return to_floating<double>(); }
-    operator float()  { return f(); }
-    operator double() { return d(); }
-    // clang-format on
-
-private:
-    template <typename F> F to_floating() const
-    {
-        uint_fast32_t up = _v;
-        up >>= 8;
-        up &= 0x00ffffff;
-        uint_fast32_t down_u = _v;
-        down_u &= 0xff;
-        F down = down_u;
-        down /= 255;
-        return up + down;
-    };
-};
 
 struct StringParser
 {
