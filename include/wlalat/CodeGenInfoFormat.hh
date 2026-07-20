554a5c0310fe22148fe18bdf8b006b908dc9a03c
Pavel Korotkevich <pawday@mail.ru>
2026-07-20 20:45:36 +0300

    Add more types to CodeGenInfoFormat.hh

--- include/wlalat/CodeGenInfoFormat.hh
+++ include/wlalat/CodeGenInfoFormat.hh
@@ -1,6 +1,7 @@
 #include "CodeGenInfo.hh"
 
 #include <format>
+#include <vector>
 
 namespace wlalat
 {
@@ -25,7 +26,107 @@ struct Formatter
 
 #undef WLALAT_FORMATTER_MAKE_METADATA_OVERLOAD_FOR
 
+#define WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(TYPE)                               \
+    constexpr auto operator()(const TYPE &M)                                   \
+    {                                                                          \
+        it = std::format_to(it, "{{");                                         \
+        it = format(M);                                                        \
+        it = std::format_to(it, "}}");                                         \
+        return it;                                                             \
+    }
+
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Protocol);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Interface);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Request);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Event);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Enum);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::EnumEntry);
+    WLALAT_FORMATTER_MAKE_OVERLOAD_FOR(CodeGen::Argument);
+
+#undef WLALAT_FORMATTER_MAKE_OVERLOAD_FOR
+
   private:
+    template <typename T>
+    constexpr auto format_array(const std::vector<T> &arr)
+    {
+        bool f = true;
+        it = std::format_to(it, "[");
+        for (auto &el : arr) {
+            if (!f) {
+                it = std::format_to(it, ",");
+            }
+            f = false;
+            it = std::format_to(it, "{{");
+            it = format(el);
+            it = std::format_to(it, "}}");
+        }
+        it = std::format_to(it, "]");
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::Protocol &O)
+    {
+        it = format_metadata_key_val(O);
+        it = std::format_to(it, ",\"interfaces\":");
+        it = format_array(O.interfaces);
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::Interface &O)
+    {
+        it = format_metadata_key_val(O);
+        it = std::format_to(it, ",\"requests\":");
+        it = format_array(O.requests);
+        it = std::format_to(it, ",\"events\":");
+        it = format_array(O.events);
+        it = std::format_to(it, ",\"enums\":");
+        it = format_array(O.enums);
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::Enum &O)
+    {
+        it = format_metadata_key_val(O);
+        it = std::format_to(it, ",\"entries\":");
+        it = format_array(O.entries);
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::Request &O)
+    {
+        it = format_metadata_key_val(O);
+        it = std::format_to(it, ",\"arguments\":");
+        it = format_array(O.args);
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::Event &O)
+    {
+        it = format_metadata_key_val(O);
+        it = std::format_to(it, ",\"arguments\":");
+        it = format_array(O.args);
+        return it;
+    }
+
+    constexpr auto format(const CodeGen::EnumEntry &O)
+    {
+        return format_metadata(O);
+    }
+
+    constexpr auto format(const CodeGen::Argument &O)
+    {
+        return format_metadata(O);
+    }
+
+    template <typename T>
+    constexpr auto format_metadata_key_val(const T &V)
+    {
+        it = std::format_to(it, "\"metadata\":{{");
+        it = format_metadata(V);
+        it = std::format_to(it, "}}");
+        return it;
+    }
+
     template <typename T>
     constexpr auto format_metadata(const T &M)
     {
@@ -87,6 +188,15 @@ WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::EventMetadata)
 WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::ArgumentMetadata)
 WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::EnumMetadata)
 WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::EnumEntryMetadata)
+
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Protocol)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Interface)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Request)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Event)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Argument)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::Enum)
+WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR(wlalat::CodeGen::EnumEntry)
+
 } // namespace std
 
 #undef WLALAT_MAKE_STD_FORMATTER_SPECIALISATION_FOR
