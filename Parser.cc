505e8cebf97f7dce8c82d84bcb117ebe16125981
Pavel Korotkevich <pawday@mail.ru>
2026-07-20 20:50:16 +0300

    Remove dump(T) from Parser.cc

--- Parser.cc
+++ Parser.cc
@@ -11,7 +11,6 @@
 #include <ios>
 #include <print>
 #include <string_view>
-#include <utility>
 #include <vector>
 
 constexpr auto test(std::string_view str)
@@ -29,102 +28,6 @@ static_assert(
     test("<protocol><interface></interface></protocol><protocol><interface></"
          "interface></protocol>") == 2);
 
-wlalat::CodeGen::LineList
-    dump(const std::vector<wlalat::CodeGen::Argument> &args)
-{
-    wlalat::CodeGen::LineList O;
-    for (auto &arg : args) {
-        auto &meta =
-            static_cast<const wlalat::CodeGen::ArgumentMetadata &>(arg);
-        O += std::format("Argument {{{}}}", meta);
-    }
-    return O;
-}
-
-wlalat::CodeGen::LineList
-    dump(const std::vector<wlalat::CodeGen::EnumEntry> &entries)
-{
-    wlalat::CodeGen::LineList O;
-    for (auto &entry : entries) {
-        auto &meta =
-            static_cast<const wlalat::CodeGen::EnumEntryMetadata &>(entry);
-        O += std::format("Entry {{{}}}", meta);
-    }
-    return O;
-}
-
-wlalat::CodeGen::LineList dump(const wlalat::CodeGen::Enum &enum_v)
-{
-    wlalat::CodeGen::LineList O;
-    auto &meta = static_cast<const wlalat::CodeGen::EnumMetadata &>(enum_v);
-    O += std::format("Enum {{{}}}", meta);
-    auto entries = dump(enum_v.entries);
-    entries.indent();
-    O += std::move(entries);
-    return O;
-}
-
-wlalat::CodeGen::LineList dump(const wlalat::CodeGen::Event &ev)
-{
-    wlalat::CodeGen::LineList O;
-    auto &meta = static_cast<const wlalat::CodeGen::EventMetadata &>(ev);
-    O += std::format("Event {{{}}}", meta);
-    auto args = dump(ev.args);
-    args.indent();
-    O += std::move(args);
-    return O;
-}
-
-wlalat::CodeGen::LineList dump(const wlalat::CodeGen::Request &req)
-{
-    wlalat::CodeGen::LineList O;
-    auto &meta = static_cast<const wlalat::CodeGen::RequestMetadata &>(req);
-    O += std::format("Request {{{}}}", meta);
-    auto args = dump(req.args);
-    args.indent();
-    O += std::move(args);
-    return O;
-}
-
-wlalat::CodeGen::LineList dump(const wlalat::CodeGen::Interface &iface)
-{
-    wlalat::CodeGen::LineList O;
-    auto &meta = static_cast<const wlalat::CodeGen::InterfaceMetadata &>(iface);
-    O += std::format("Interface {{{}}}", meta);
-
-    for (auto &event : iface.events) {
-        auto E = dump(event);
-        E.indent();
-        O += std::move(E);
-    }
-
-    for (auto &request : iface.requests) {
-        auto R = dump(request);
-        R.indent();
-        O += std::move(R);
-    }
-
-    for (auto &enum_v : iface.enums) {
-        auto E = dump(enum_v);
-        E.indent();
-        O += std::move(E);
-    }
-    return O;
-}
-
-wlalat::CodeGen::LineList dump(const wlalat::CodeGen::Protocol &proto)
-{
-    wlalat::CodeGen::LineList O;
-    auto &meta = static_cast<const wlalat::CodeGen::ProtocolMetadata &>(proto);
-    O += std::format("Protocol {{{}}}", meta);
-    for (auto &iface : proto.interfaces) {
-        auto I = dump(iface);
-        I.indent();
-        O += std::move(I);
-    }
-    return O;
-}
-
 int main(int argc, char **argv)
 try {
     std::ifstream file{argv[1]};
@@ -140,12 +43,16 @@ try {
 
     auto protos_collect = p.parse();
 
+    bool f = true;
+    std::print("[");
     for (auto &proto : protos_collect) {
-        auto lines = dump(proto);
-        for (auto &line : lines) {
-            std::println("{}", line);
+        if (!f) {
+            std::print(",");
         }
+        f = false;
+        std::print("{}", proto);
     }
+    std::print("]");
 
 } catch (std::exception &e) {
     std::println("std::exception::what() [{}]", e.what());
